390b905146e9979b9012de71ac011d31918fa184
Nicolas <Nicolas.Babel@enseirb-matmeca.fr>
2019-01-16 16:08:22 +0100

    added data generator

--- main.py
+++ main.py
@@ -9,12 +9,17 @@ from keras.layers import LeakyReLU, Dropout
 from keras.layers import BatchNormalization
 from keras.optimizers import Adam, RMSprop
 
+from keras.preprocessing.image import ImageDataGenerator
+from random import randint
+
+import matplotlib.pyplot as plt
+
 
 class Gan():
     def __init__(self, img_rows = 28, img_cols = 28, channels = 1):
         self.img_rows = img_rows
         self.img_cols = img_cols
-        self.channel = channel
+        self.channels = channels
         self.D = None   # discriminator
         self.G = None   # generator
         self.AM = None  # adversarial model
@@ -41,13 +46,25 @@ class filmPosterGan():
         self.img_rows = rows
         self.img_cols = cols
         self.channels = channels
+        self.dataFolder = dataFolder
         # TODO : create a data generator and read files from dataFolder in order to generate train set and validation set
         # maybe use one hot encoding for the class
         self.posters = None
 
         self.gan = Gan()
+    def load_data(self, grayscale = True):
 
-    def train(self, train_steps=500, batch_size=256)
+        dataGen = ImageDataGenerator()
+        data_set = dataGen.flow_from_directory(
+            self.dataFolder,
+            target_size = (self.img_rows, self.img_cols),
+            batch_size = 32,
+            class_mode = 'categorical',
+            color_mode = 'grayscale' if grayscale else 'rgb'
+        )
+        return data_set
+
+    def train(self, train_steps=500, batch_size=256):
         
         for i in range(train_steps):
             noise_input = np.random.uniform(-1.0, 1.0, size=[batch_size,100]) #Fake image will be generated from a noise TODO : make sure the noise shape is OK
@@ -56,3 +73,24 @@ class filmPosterGan():
 
 
             #TODO : Generate a new noise and train the adversarial model with label 1
+
+grayscale = True
+fpg = filmPosterGan(rows = 64, cols = 64, channels = 1, dataFolder = "./data")
+data_gen = fpg.load_data()
+print(data_gen)
+(x, y) = data_gen.next()
+print(x.shape)
+
+i = randint(1,31)
+print(x[i].shape)
+image = x[i]
+if grayscale: image = np.reshape(image, (64, 64))
+print("Dimension of input image=",np.shape(image))
+
+#plt.imshow(image.transpose(2,1,0))
+if i>0: plt.figure()
+if grayscale:
+    plt.imshow(image, cmap='gray')
+else:
+    plt.imshow(image)
+plt.show()
